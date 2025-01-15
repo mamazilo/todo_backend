5c33e647a1a6c86bdb7773f5a43216f8ea9cea27
mamazilo <mohamadrezalotfalizadeh@yahoo.com>
2025-01-15 13:11:46 +0330

    add changes

--- apps/notes/urls.py
+++ apps/notes/urls.py
@@ -1,16 +1,16 @@
 from django.urls import path, include
 from . import views
-from rest_framework.routers import DefaultRouter
+# from rest_framework.routers import DefaultRouter
 
-router = DefaultRouter()
-router.register('', views.Note)
+# router = DefaultRouter()
+# router.register('', views.Note)
 
 urlpatterns = [
-    path('funcnote/', views.note),
-    path('funcnote/<int:todo_id>', views.note_detail_view),
-    path('note/', views.NoteList.as_view()),
-    path('note/<int:todo_id>', views.NoteEditeList.as_view()),
-    path('genericnote/', views.NoteList.as_view()),
-    path('genericnote/<pk>', views.EditeNote.as_view()),
-    path('viewnote/', include(router.urls)),
+    # path('funcnote/', views.note),
+    # path('funcnote/<int:todo_id>', views.note_detail_view),
+    path('', views.NoteList.as_view()),
+    path('<int:todo_id>', views.NoteEditeList.as_view()),
+    # path('genericnote/', views.NoteList.as_view()),
+    # path('genericnote/<pk>', views.EditeNote.as_view()),
+    # path('viewnote/', include(router.urls)),
 ]
--- config/urls.py
+++ config/urls.py
@@ -20,6 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
-    path('', include('apps.home.urls')),
-    path('api/', include('apps.notes.urls')),
+    path('', include('apps.notes.urls')),
 ]
